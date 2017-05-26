ec3a36e6e832d6da06ba6280a13e4bd862f0348d
Atzin <camargoescandia@gmail.com>
2017-05-26 08:52:16 +0200

    first part structure ex modal

---
+++ js/main.js
@@ -0,0 +1,16 @@
+<div class="pop-up">
+      <div class="window">
+        <p class="instructions">Instrucciones</p>
+        <p class="rules">Reglas:</p>
+        <div class="containerRules">
+         <p class="poprInstr">1. Escribe un número entre 1 y 99</p>
+         <p class="poprInstr">2. Haz click en el botón dale caña para comprobar</p>
+         <p class="poprInstr">3. Sigue las pistas que te aparecen</p>
+         <p class="poprInstr">4. Elige un nuevo número hasta que aciertes</p>
+         <p class="poprInstr">¡Suerte!</p>
+        </div>
+        <div class="buttonInstructions" id="button-instructions">
+            <p class="button1" id="close">Lets Daba-do-it!</p>
+        </div>
+      </div>
+    </div>
